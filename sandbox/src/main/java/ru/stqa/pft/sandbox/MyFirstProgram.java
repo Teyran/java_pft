f14252af67e9a8167c9aeca81c54dc6c5f1fdd22
Teyran <teyran1993@mail.ru>
2020-01-08 21:52:12 +0300

    Домашнее задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,7 +12,13 @@ public class MyFirstProgram {
 
     Rectangle rectangle = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + rectangle.area());
+
+    Point pointAB = new Point(3.f, 4.f, 7.f, 1.f);
+    System.out.println("Расстояние между двумя заданными точками равно " + pointAB.distanceBetweenTwoPoints());
+
+
   }
+
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -8,6 +8,12 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square square = new Square(5);
-    Assert.assertEquals(square.area(),  24);
+    Assert.assertEquals(square.area(),  25);
+  }
+
+  @Test
+  public void distanceBetweenPoints() {
+    Point pointAC = new Point(3,4,7,1);
+    Assert.assertEquals(pointAC.distanceBetweenTwoPoints(), 5);
   }
 }
