d68b2aaf5b6a889db3d394e34b65aba452e53be2
Teyran <teyran1993@mail.ru>
2020-01-08 18:12:26 +0300

    Выполнен рефакторинг классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,20 +8,14 @@ public class MyFirstProgram {
     hello("Teyran");
 
     Square square = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + square.l + " = " + area(square));
+    System.out.println("Площадь квадрата со стороной " + square.l + " = " + square.area());
 
     Rectangle rectangle = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + area(rectangle));
+    System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + rectangle.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-  public static double area (Square s) {
-    return s.l * s.l;
-  }
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 
 
