2c47390dcff4b678c153e2e0f0cce9be427568a7
Teyran <teyran1993@mail.ru>
2020-02-04 09:16:02 +0300

    Добавлен базовый класс для классов помощников

--- adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -3,14 +3,14 @@ package ru.stqa.pft.adressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.pft.adressbook.model.GroupData;
 
-public class GroupCreationTests extends TestBase {
+public class  GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.goToGroupPage();
-    app.initGroupCreation();
-    app.fillGroupForm(new GroupData("TestGroup1", "TestGroup2", "TestGroup3"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "TestGroup2", "TestGroup3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
   }
 }
--- adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
+++ adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
-  private WebDriver wd;
+
 
   @Test
   public void testGroupDeletion() throws Exception {
-    app.goToGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroups();
-    app.returnToGroupPage();
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }
