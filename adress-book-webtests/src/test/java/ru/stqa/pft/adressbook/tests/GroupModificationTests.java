ae403150f0ca3724e8b446a9501f35ec266165e6
Teyran <teyran1993@mail.ru>
2020-02-04 09:36:26 +0300

    Добавлен тест модификации группы

---
+++ adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "TestGroup2", "TestGroup3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
