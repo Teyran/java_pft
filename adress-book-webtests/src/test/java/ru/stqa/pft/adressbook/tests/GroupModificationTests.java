23201affadb03d71be562f76845e1e647aa068f3
Teyran <teyran1993@mail.ru>
2020-02-21 15:01:13 +0300

    Добавлен выбор элемента Группы по порядковому номеру

--- adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
+++ adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -12,7 +12,7 @@ public class GroupModificationTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("TestGroup1","TestGroup2","TestGroup3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "Modified2", "Modified3"));
     app.getGroupHelper().submitGroupModification();
