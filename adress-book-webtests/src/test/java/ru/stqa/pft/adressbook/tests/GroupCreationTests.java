1c59741b7700aaeab5bda909119c31c689963697
Teyran <teyran1993@mail.ru>
2020-03-01 18:29:17 +0300

    Добавлено вычисление максимального идентификатора группы через лямда-выражение

--- adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adress-book-webtests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -4,9 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.adressbook.model.GroupData;
 
-import javax.management.MBeanRegistration;
 import java.util.HashSet;
 import java.util.List;
+
+
 
 public class  GroupCreationTests extends TestBase {
 
@@ -15,18 +16,18 @@ public class  GroupCreationTests extends TestBase {
 
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getgroupList();
-    GroupData group = new GroupData("TestGroup1", null, null);
-    app.getGroupHelper().createGroup(new GroupData("TestGroup1",null,null));
+    GroupData group = new GroupData("TestGroup2", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getgroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g: after) {
-      if(g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+//    int max = 0;
+//    for (GroupData g: after) {
+//      if(g.getId() > max) {
+//        max = g.getId();
+//      }
+//    }
+    group.setId(after.stream().max((groupData, t1) -> Integer.compare(groupData.getId(), t1.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
